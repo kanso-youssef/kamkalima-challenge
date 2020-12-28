62454c3ad30f1054ec73a84f3ed1ed6458a55c1d
Youssef Kanso <youssef_kanso@outlook.com>
2020-12-28 22:05:52 +0200

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
 const path = require('path');
- 
+
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, './src/index.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
